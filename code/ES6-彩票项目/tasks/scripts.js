4426ba4f03a28b8ef2f4ebd85c1b49683d721b28
MrYz912 <1600332850@qq.com>
2019-09-10 15:22:31 +0800

    a

---
+++ code/ES6-彩票项目/tasks/scripts.js
@@ -0,0 +1,34 @@
+import gulp from "gulp";
+import gulpif from "gulp-if";//if语句
+import concat from "gulp-concat";//合并
+import webpack from "webpack";
+import gulpwebpack from "webpack-stream";
+import named from "vinyl-named";//命名
+import livereload from "gulp-livereload";//热更新
+import plumber from "gulp-plumber";//处理文件信息流
+import rename from "gulp-rename";//重命名
+import uglify from "gulp-uglify";//处理压缩
+import {log,colors} from "gulp-util";//在命令行输出
+import args from "./util/args";
+
+gulp.task( "scripts",() => {
+    return gulp.src(['app/js/index.js'])
+        .pipe(plumber({
+            errorHandler:function(){
+
+            }
+        }))
+        .pipe(named())
+        .pipe(gulpwebpack({
+            module:{
+                loaders:[{
+                    test:/\.js$/,
+                    loader: "babel"
+                }]
+            }
+        }),null,(err, stats)=>{
+            log(`Finished '${colors.cyan('scripts')}'`,stats.toString({
+                chunks:false
+            }))
+        })
+})
