0a38d1d7cda85317cdefc5fceb63f6624d921169
MrYz912 <1600332850@qq.com>
2019-09-22 16:20:45 +0800

    a

--- code/ES6-彩票项目/server/public/js/index.js
+++ code/ES6-彩票项目/server/public/js/index.js
@@ -51,15 +51,7 @@
 /* 1 */
 /***/ (function(module, exports) {
 
-	class Test {
-	    constructor(){
-	        this.a = "hello world!"
-	    }
-	}
-
-	let test = new Test();
-
-	document.body.innerHTML = test.a;
+	import "./class/lesson1";
 
 /***/ })
 /******/ ]);
