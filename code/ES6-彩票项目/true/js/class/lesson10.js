5559f876378c23df49d291b4aaa6fc74ec27f5a3
MrYz912 <1600332850@qq.com>
2020-02-27 22:19:50 +0800

    set

--- code/ES6-彩票项目/true/js/class/lesson10.js
+++ code/ES6-彩票项目/true/js/class/lesson10.js
@@ -53,3 +53,61 @@
 
     console.log('weakList',weakList);
 }
+
+{
+    let map = new Map();//key可以为任何数据类型的Object
+    let arr = ['123'];
+
+    map.set(arr,456);//key,value key可以为任何数据类型
+    console.log('map',map,map.get(arr));
+}
+
+{
+    let map = new Map([['a',123],['b',456]]);//遍历和Set一样
+    console.log('map_args',map);
+    console.log('size',map.size);
+    console.log('delete',map.delete('a'));
+    console.log('clear',map.clear());
+}
+
+{
+    let weakmap = new WeakMap();//WeakMap和Map的关系与WeakSet与Set关系一样
+    let o = {};
+    weakmap.set(o,123);
+    console.log(weakmap.get(o));
+}
+
+{//数据结构横向对比，增删查改
+    let map = new Map();
+    let arr = [];
+    //增
+    map.set('t',1);
+    arr.push({t:1});
+
+    console.info('map-arr',map, arr);
+    //查
+    let map_exist = map.has('t');
+    let arr_exist = arr.find(item => item.t);
+    
+    console.info('map-arr',map_exist,arr_exist);
+    //改
+    map.set('t',2);
+    arr.forEach(item=>item.t ? item.t=2 : '');
+    
+    console.info('map-arr',map, arr);
+    //删
+    map.delete("t");
+    let index = arr.findIndex(item=>item.t);
+    arr.splice(index,1);
+    
+    console.info('map-array-empty',map,arr);
+}
+
+{//set和array的对比
+    let set = new Set();
+    let array = [];
+
+    set.add({t:1});
+    array.push({t:1});
+    console.info('set-array',set,array);
+}
